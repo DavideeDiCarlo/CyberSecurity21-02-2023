d519525aefeafb8163b1c5a184d1eec5c424a977
DavideeDiCarlo <126014909+DavideeDiCarlo@users.noreply.github.com>
2023-05-17 19:32:03 +0200

    Delete calcolo caratteri.py

--- calcolo caratteri.py
+++
@@ -1,7 +0,0 @@
-#data una lista d'ingresso A contenente N parole, restituisce una lista b di interi che rappresentano la lunghezza delle parole in A
-def contacaratteri (lunghezza):
-  lunghezza = x
-  print(len(x))
-
-x = input ("Conta i caratteri del tuo testo: ")
-contacaratteri(x) 
